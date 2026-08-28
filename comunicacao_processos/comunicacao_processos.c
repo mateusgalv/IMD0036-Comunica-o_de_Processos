f3f17b56a6c93211cc611ae69648dcc8dee333d0
Mateus Galvao <mateustgalvao@outlook.com>
2026-08-28 16:55:24 -0300

    exercicio comunicacao de processos

--- comunicacao_processos/comunicacao_processos.c
+++ comunicacao_processos/comunicacao_processos.c
@@ -6,11 +6,11 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-void implentacao_filho1 (int *num) {
+void implementacao_filho1 (int *num) {
     *num = *num + 1;
 }
 
-void implentacao_filho2 (int *num) {
+void implementacao_filho2 (int *num) {
     *num = *num * 2;
 }
 
@@ -18,43 +18,67 @@ int main () {
     int *mem;
     pid_t filho1, filho2;
 
+    // shmget "pega" um segmento de memória compartilhada
+    // shmget retorna um identificador
     int seg_id = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    // shmat "pega" esse segmento e o mapeia no espaço de endereçamento do processo atual
+    // shmat retorna um ponteiro
     mem = shmat(seg_id, NULL, 0); /*anexa memória compartilhada*/
+    /*
+    void *shmat(int shmid, const void *shmaddr, int shmflg);
+    shmid -> identificador -> retornado por shmget (seg_id)
+    shmaddr -> NULL = Não precisa escolher manualmente o endereço
+    shmflg -> 0 = flag padrão de leitura/escrita
+    */
 
-    *mem = 10; /*escreve inteiro qualquer no segmento de memória compartilhada*/
+    // inteiro qualquer escrito no segmento de memória compartilhada
+    *mem = 10;
 
-    printf("Processo PAI iniciado - PID: %d, inteiro: %d\n", getpid(), *mem);
+    printf("Processo PAI iniciado - PID: %d\n", getpid());
+    printf("Inteiro armazenado na memória compartilhada: %d\n", *mem);
 
     filho1 = fork();
+
     if (filho1 == 0) {
-        printf("Processo filho1 iniciado - PID: %d\n", getpid());
-        mem = shmat(seg_id, NULL, 0);
-        implentacao_filho1(mem);
-        printf("filho1 finalizou - PID: %d - inteiro: %d\n", getpid(), *mem);
+        printf("Processo FILHO_1 iniciado - PID: %d\n", getpid());
+        printf("Inteiro armazenado na memória compartilhada: %d\n", *mem);
+        implementacao_filho1(mem);
+        printf("Processo FILHO_1 terminado - PID: %d\n", getpid());
+        printf("Inteiro armazenado na memória compartilhada: %d\n", *mem);
         
-        shmdt(mem); /*desanexa memória compartilhada*/
+        // desanexa memória compartilhada
+        shmdt(mem);
+        
         exit(0);
     }
     
     waitpid(filho1, NULL, 0);
+    printf("Processo PAI update - PID: %d\n", getpid());
+    printf("Inteiro armazenado na memória compartilhada: %d\n", *mem);
     filho2 = fork();
     
     if (filho2 == 0) {
-        printf("Processo filho2 criado - PID: %d\n", getpid());
-        mem = shmat(seg_id, NULL, 0);
-        implentacao_filho2(mem);
-        printf("filho2 finalizou - PID: %d - inteiro: %d\n", getpid(), *mem);
+        printf("Processo FILHO_2 iniciado - PID: %d\n", getpid());
+        printf("Inteiro armazenado na memória compartilhada: %d\n", *mem);
+        implementacao_filho2(mem);
+        printf("Processo FILHO_2 terminado - PID: %d\n", getpid());
+        printf("Inteiro armazenado na memória compartilhada: %d\n", *mem);
 
+        // desanexa memória compartilhada
         shmdt(mem);
+
         exit(0);
-    } else {
-        waitpid(filho2, NULL, 0);
-
-        printf("Processo PAI finalizado, inteiro armazenado: %d\n", *mem);
-        
-        shmdt(mem);
-        shmctl(seg_id, IPC_RMID, NULL); /*remove o segmento do sistema*/
     }
+
+    waitpid(filho2, NULL, 0);
+    printf("Processo PAI terminado - PID: %d\n", getpid());
+    printf("Inteiro armazenado na memória compartilhada: %d\n", *mem);
+    
+    // desanexa memória compartilhada
+    shmdt(mem);
+
+    // remove o segmento do sistema
+    shmctl(seg_id, IPC_RMID, NULL);
 
     return 0;
 }
